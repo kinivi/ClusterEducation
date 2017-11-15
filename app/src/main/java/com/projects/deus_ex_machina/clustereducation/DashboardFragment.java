58a8f1d799f575aa5da8e253823083974a9ee0cd
Nikita Kiselov <nikita.kiselov@outlook.com>
2017-11-16 00:27:18 +0200

    Added card for Chart
    Added card for Chart and simple pie chart

--- app/src/main/java/com/projects/deus_ex_machina/clustereducation/DashboardFragment.java
+++ app/src/main/java/com/projects/deus_ex_machina/clustereducation/DashboardFragment.java
@@ -1,17 +1,30 @@
 package com.projects.deus_ex_machina.clustereducation;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DashboardFragment extends Fragment {
+
+    private PieChart mChart;
 
 
     public DashboardFragment() {
@@ -22,8 +35,61 @@ public class DashboardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        mChart = (PieChart) rootView.findViewById(R.id.pieChart);
+        mChart.setBackgroundColor(Color.WHITE);
+
+        mChart.setUsePercentValues(true);
+        mChart.getDescription().setEnabled(false);
+
+        mChart.setCenterText("Hello World");
+
+        mChart.setDrawHoleEnabled(true);
+        mChart.setHoleColor(Color.WHITE);
+
+        mChart.setTransparentCircleColor(Color.WHITE);
+        mChart.setTransparentCircleAlpha(110);
+
+        mChart.setHoleRadius(58f);
+        mChart.setTransparentCircleRadius(61f);
+
+        mChart.setDrawCenterText(true);
+
+        mChart.setRotationEnabled(false);
+        mChart.setHighlightPerTapEnabled(true);
+
+        mChart.setMaxAngle(180f); // HALF CHART
+        mChart.setRotationAngle(180f);
+        setData(4, 100);
+
+        mChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
+
+
+        return rootView;
+    }
+
+    private void setData(int count , int range) {
+        ArrayList<PieEntry> values = new ArrayList<PieEntry>();
+
+        for (int i = 0; i < count; i++) {
+            values.add(new PieEntry((float) ((Math.random() * range) + range / 5)));
+        }
+
+        PieDataSet dataSet = new PieDataSet(values, "Election Results");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        //dataSet.setSelectionShift(0f);
+
+        PieData data = new PieData(dataSet);
+        data.setValueFormatter(new PercentFormatter());
+        data.setValueTextSize(11f);
+        data.setValueTextColor(Color.WHITE);
+        mChart.setData(data);
+
+        mChart.invalidate();
     }
 
 }
