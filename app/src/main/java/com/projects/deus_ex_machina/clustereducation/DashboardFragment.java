466aefbd7765c727fb043a14a90035543e75330b
Nikita Kiselov <nikita.kiselov@outlook.com>
2017-11-19 18:00:23 +0200

    Redisign the layout

--- app/src/main/java/com/projects/deus_ex_machina/clustereducation/DashboardFragment.java
+++ app/src/main/java/com/projects/deus_ex_machina/clustereducation/DashboardFragment.java
@@ -3,7 +3,6 @@ package com.projects.deus_ex_machina.clustereducation;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,16 +12,20 @@ import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.formatter.DefaultValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
+
+import static com.github.mikephil.charting.utils.ColorTemplate.rgb;
 
 
 /**
@@ -30,7 +33,7 @@ import java.util.ArrayList;
  */
 public class DashboardFragment extends Fragment {
 
-    private PieChart mChart;
+    private PieChart mPieChart;
     private HorizontalBarChart mBarChart;
 
 
@@ -42,41 +45,86 @@ public class DashboardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //Getting the rootView to access standard methods of Activity in Fragment
         View rootView = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
-        ConstraintLayout constraintLayout = rootView.findViewById(R.id.pieLayout);
-        int height = constraintLayout.getMaxHeight();
-
-
-        mChart = (PieChart) rootView.findViewById(R.id.pieChart);
+        //Getting ID's of 2 charts
+        mPieChart = (PieChart) rootView.findViewById(R.id.pieChart);
         mBarChart = (HorizontalBarChart) rootView.findViewById(R.id.horizontalBarChart);
-        mChart.setBackgroundColor(Color.WHITE);
 
-        mChart.setUsePercentValues(true);
-        mChart.getDescription().setEnabled(false);
+        mPieChart.setBackgroundColor(Color.WHITE);
+        mBarChart.setBackgroundColor(Color.WHITE);
 
-        //mChart.setCenterText("Hello World");
+        mPieChart.setUsePercentValues(true);
+        mPieChart.getDescription().setEnabled(false);
 
-        mChart.setDrawHoleEnabled(false);
-        //mChart.setHoleColor(Color.WHITE);
+        //mPieChart.setCenterText("The worst subjects");
 
-        //mChart.setTransparentCircleColor(Color.WHITE);
-        //mChart.setTransparentCircleAlpha(110);
+        mPieChart.setDrawHoleEnabled(false);
+        //mPieChart.setHoleColor(Color.WHITE);
 
-        //mChart.setHoleRadius(55f);
-        //mChart.setTransparentCircleRadius(58f);
-        Legend l = mChart.getLegend();
+        //mPieChart.setTransparentCircleColor(Color.WHITE);
+        //mPieChart.setTransparentCircleAlpha(110);
+
+        //mPieChart.setHoleRadius(55f);
+        //mPieChart.setTransparentCircleRadius(58f);
+        Legend l = mPieChart.getLegend();
+        l.setTextSize(15f);
+        l.setForm(Legend.LegendForm.CIRCLE);
         l.setVerticalAlignment(Legend.LegendVerticalAlignment.CENTER);
         l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
         l.setOrientation(Legend.LegendOrientation.VERTICAL);
-        mChart.setDrawCenterText(true);
+        mPieChart.setDrawCenterText(true);
 
-        mChart.setHighlightPerTapEnabled(true);
+        mPieChart.setHighlightPerTapEnabled(true);
 
-        setData(4, 100);
-        setDataForBarChart(4,100);
+        YAxis yAxis = mBarChart.getAxisRight();
+        yAxis.setEnabled(false);
 
-        mChart.animateY(1500, Easing.EasingOption.EaseOutQuart);
+        yAxis = mBarChart.getAxisLeft();
+        yAxis.setEnabled(false);
+
+        XAxis xAxis = mBarChart.getXAxis();
+        xAxis.setEnabled(false);
+
+        mBarChart.setDrawValueAboveBar(true);
+
+        // scaling can now only be done on x- and y-axis separately
+        mBarChart.setPinchZoom(false);
+        mBarChart.setDrawGridBackground(false);
+
+        mBarChart.getDescription().setEnabled(false);
+
+        // scaling can now only be done on x- and y-axis separately
+        mBarChart.setPinchZoom(false);
+
+
+
+
+
+
+        YAxis yl = mBarChart.getAxisLeft();
+        yl.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+//        yl.setInverted(true);
+
+
+        Legend l2 = mBarChart.getLegend();
+        l2.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l2.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        l2.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l2.setDrawInside(false);
+        l2.setFormSize(10f);
+        l2.setTextSize(10f);
+
+        l2.setFormToTextSpace(4f);
+        l2.setXEntrySpace(6f);
+
+
+
+        setDataForPieChart(4, 5);
+        setDataForBarChart(4,80);
+
+        mPieChart.animateY(1500, Easing.EasingOption.EaseOutQuart);
         mBarChart.animateY(1500, Easing.EasingOption.EaseOutQuart);
 
 
@@ -85,14 +133,14 @@ public class DashboardFragment extends Fragment {
         return rootView;
     }
 
-    private void setData(int count , int range) {
+    private void setDataForPieChart(int count , int range) {
         ArrayList<PieEntry> values = new ArrayList<PieEntry>();
 
         for (int i = 0; i < count; i++) {
-            values.add(new PieEntry((float) ((Math.random() * range) + range / 5), "Quarter"));
+            values.add(new PieEntry((float) ((Math.random() * range) + range / 5), "Good"));
         }
 
-        PieDataSet dataSet = new PieDataSet(values, "Election Results");
+        PieDataSet dataSet = new PieDataSet(values, "Mark of program");
         dataSet.setSliceSpace(3f);
         dataSet.setSelectionShift(5f);
 
@@ -100,27 +148,38 @@ public class DashboardFragment extends Fragment {
         //dataSet.setSelectionShift(0f);
 
         PieData data = new PieData(dataSet);
-        data.setValueFormatter(new PercentFormatter());
-        data.setValueTextSize(11f);
+        data.setValueFormatter(new DefaultValueFormatter(0));
+        data.setValueTextSize(17f);
         data.setValueTextColor(Color.WHITE);
-        mChart.setData(data);
+        mPieChart.setData(data);
 
-        mChart.invalidate();
+        mPieChart.invalidate();
     }
 
     private void setDataForBarChart(int count , int range) {
-        ArrayList<BarEntry> values = new ArrayList<BarEntry>();
+
+        BarData data = new BarData();
+
+        int[] MATERIAL_COLORS = {
+                rgb("#2ecc71"), rgb("#f1c40f"), rgb("#e74c3c"), rgb("#3498db")
+        };
 
         for (int i = 0; i < count; i++) {
-            values.add(new BarEntry(i+'f',(float) ((Math.random() * range) + range / 5), "Quarter"));
+            ArrayList<BarEntry> value = new ArrayList<BarEntry>();
+            value.add(new BarEntry(i+'f',(float) ((Math.random() * range) + range / 5)));
+
+            BarDataSet dataSet = new BarDataSet(value,"Subject #" + i);
+            dataSet.setColors(MATERIAL_COLORS[i]);
+            data.addDataSet(dataSet);
+
         }
 
-        BarDataSet dataSet = new BarDataSet(values, "Election Results");
-        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+
+
         //dataSet.setSelectionShift(0f);
 
-        BarData data = new BarData(dataSet);
-        data.setValueFormatter(new PercentFormatter());
+
+        //data.setValueFormatter(new PercentFormatter());
         data.setValueTextSize(11f);
         //data.setValueTextColor(Color.WHITE);
         mBarChart.setData(data);
