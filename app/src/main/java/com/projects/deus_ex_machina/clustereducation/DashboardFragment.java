33f871b0c4319067c608f6b1b5af8b8d93470b8f
Nikita Kiselov <nikita.kiselov@outlook.com>
2017-11-20 00:29:59 +0200

    Polish the structure of the code
    move all calls that sets Chart appearance into one method for each chart

--- app/src/main/java/com/projects/deus_ex_machina/clustereducation/DashboardFragment.java
+++ app/src/main/java/com/projects/deus_ex_machina/clustereducation/DashboardFragment.java
@@ -2,6 +2,7 @@ package com.projects.deus_ex_machina.clustereducation;
 
 
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -20,7 +21,7 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.formatter.DefaultValueFormatter;
+import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
@@ -52,88 +53,21 @@ public class DashboardFragment extends Fragment {
         mPieChart = (PieChart) rootView.findViewById(R.id.pieChart);
         mBarChart = (HorizontalBarChart) rootView.findViewById(R.id.horizontalBarChart);
 
-        mPieChart.setBackgroundColor(Color.WHITE);
-        mBarChart.setBackgroundColor(Color.WHITE);
-
-        mPieChart.setUsePercentValues(true);
-        mPieChart.getDescription().setEnabled(false);
-
-        //mPieChart.setCenterText("The worst subjects");
-
-        mPieChart.setDrawHoleEnabled(false);
-        //mPieChart.setHoleColor(Color.WHITE);
-
-        //mPieChart.setTransparentCircleColor(Color.WHITE);
-        //mPieChart.setTransparentCircleAlpha(110);
-
-        //mPieChart.setHoleRadius(55f);
-        //mPieChart.setTransparentCircleRadius(58f);
-        Legend l = mPieChart.getLegend();
-        l.setTextSize(15f);
-        l.setForm(Legend.LegendForm.CIRCLE);
-        l.setVerticalAlignment(Legend.LegendVerticalAlignment.CENTER);
-        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
-        l.setOrientation(Legend.LegendOrientation.VERTICAL);
-        mPieChart.setDrawCenterText(true);
-
-        mPieChart.setHighlightPerTapEnabled(true);
-
-        YAxis yAxis = mBarChart.getAxisRight();
-        yAxis.setEnabled(false);
-
-        yAxis = mBarChart.getAxisLeft();
-        yAxis.setEnabled(false);
-
-        XAxis xAxis = mBarChart.getXAxis();
-        xAxis.setEnabled(false);
-
-        mBarChart.setDrawValueAboveBar(true);
-
-        // scaling can now only be done on x- and y-axis separately
-        mBarChart.setPinchZoom(false);
-        mBarChart.setDrawGridBackground(false);
-
-        mBarChart.getDescription().setEnabled(false);
-
-        // scaling can now only be done on x- and y-axis separately
-        mBarChart.setPinchZoom(false);
+        setPieChartAppearance();
+        setBarChartAppearance();
 
 
-
-
-
-
-        YAxis yl = mBarChart.getAxisLeft();
-        yl.setAxisMinimum(0f); // this replaces setStartAtZero(true)
-//        yl.setInverted(true);
-
-
-        Legend l2 = mBarChart.getLegend();
-        l2.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
-        l2.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
-        l2.setOrientation(Legend.LegendOrientation.HORIZONTAL);
-        l2.setDrawInside(false);
-        l2.setFormSize(10f);
-        l2.setTextSize(10f);
-
-        l2.setFormToTextSpace(4f);
-        l2.setXEntrySpace(6f);
-
-
-
-        setDataForPieChart(4, 5);
-        setDataForBarChart(4,80);
+        setDataForPieChart(4, 85);
+        setDataForBarChart(4, 80);
 
         mPieChart.animateY(1500, Easing.EasingOption.EaseOutQuart);
         mBarChart.animateY(1500, Easing.EasingOption.EaseOutQuart);
 
 
-
-
         return rootView;
     }
 
-    private void setDataForPieChart(int count , int range) {
+    private void setDataForPieChart(int count, int range) {
         ArrayList<PieEntry> values = new ArrayList<PieEntry>();
 
         for (int i = 0; i < count; i++) {
@@ -148,15 +82,17 @@ public class DashboardFragment extends Fragment {
         //dataSet.setSelectionShift(0f);
 
         PieData data = new PieData(dataSet);
-        data.setValueFormatter(new DefaultValueFormatter(0));
+        data.setValueFormatter(new PercentFormatter());
         data.setValueTextSize(17f);
         data.setValueTextColor(Color.WHITE);
+        data.setValueTypeface(Typeface.DEFAULT_BOLD);
+
         mPieChart.setData(data);
 
         mPieChart.invalidate();
     }
 
-    private void setDataForBarChart(int count , int range) {
+    private void setDataForBarChart(int count, int range) {
 
         BarData data = new BarData();
 
@@ -166,25 +102,89 @@ public class DashboardFragment extends Fragment {
 
         for (int i = 0; i < count; i++) {
             ArrayList<BarEntry> value = new ArrayList<BarEntry>();
-            value.add(new BarEntry(i+'f',(float) ((Math.random() * range) + range / 5)));
+            value.add(new BarEntry(i + 'f', (float) ((Math.random() * range) + range / 5)));
 
-            BarDataSet dataSet = new BarDataSet(value,"Subject #" + i);
+            BarDataSet dataSet = new BarDataSet(value, "Subject #" + i);
             dataSet.setColors(MATERIAL_COLORS[i]);
             data.addDataSet(dataSet);
 
         }
 
-
-
-        //dataSet.setSelectionShift(0f);
-
-
-        //data.setValueFormatter(new PercentFormatter());
         data.setValueTextSize(11f);
-        //data.setValueTextColor(Color.WHITE);
         mBarChart.setData(data);
 
         mBarChart.invalidate();
+    }
+
+    private void setPieChartAppearance() {
+        Legend l = mPieChart.getLegend();
+
+        mPieChart.setBackgroundColor(Color.WHITE);
+
+        //Setting that Pie Chart will use percent values
+        mPieChart.setUsePercentValues(true);
+
+        //Disable the description
+        mPieChart.getDescription().setEnabled(false);
+
+        //Don't draw hole inside Pie chart
+        mPieChart.setDrawHoleEnabled(false);
+        //Don't draw labels on PieChart
+        mPieChart.setDrawEntryLabels(false);
+
+        //Setting the appearance of the legend
+        l.setTextSize(15f);
+        l.setForm(Legend.LegendForm.CIRCLE);
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.CENTER);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+    }
+
+    private void setBarChartAppearance() {
+        mBarChart.setBackgroundColor(Color.WHITE);
+
+        mBarChart.setHighlightPerTapEnabled(false);
+
+        //Disable drawing all axis  of chart
+        YAxis yAxis = mBarChart.getAxisRight();
+        yAxis.setEnabled(false);
+
+        yAxis = mBarChart.getAxisLeft();
+        yAxis.setEnabled(false);
+
+        XAxis xAxis = mBarChart.getXAxis();
+        xAxis.setEnabled(false);
+
+        // scaling can now only be done on x- and y-axis separately
+        mBarChart.setPinchZoom(false);
+
+        //Disable drawing grid on the background
+        mBarChart.setDrawGridBackground(false);
+
+        //Disable the description
+        mBarChart.getDescription().setEnabled(false);
+
+        Legend l = mBarChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+
+        //sets whether the legend will draw inside the chart or outside
+        l.setDrawInside(false);
+
+        l.setFormSize(10f);
+        l.setTextSize(10f);
+
+        //sets the space between the form and the actual label/text, converts to dp internally
+        l.setFormToTextSpace(4f);
+
+        //sets the space between the legend entries on a horizontal axis in pixels,
+        //converts to dp internally
+        l.setXEntrySpace(8f);
+
+        //Set axis minimum to 0 and it provides labels under each Bar Chart
+        YAxis y = mBarChart.getAxisLeft();
+        y.setAxisMinimum(0f);
     }
 
 }
