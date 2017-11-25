64a8c60cc104217c5f520328b16ad4e15eb3f780
Nikita Kiselov <nikita.kiselov@outlook.com>
2017-11-25 14:00:38 +0200

    Connected results to Firebase

--- app/src/main/java/com/projects/deus_ex_machina/clustereducation/PollActivity.java
+++ app/src/main/java/com/projects/deus_ex_machina/clustereducation/PollActivity.java
@@ -1,13 +1,85 @@
 package com.projects.deus_ex_machina.clustereducation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
 
 public class PollActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poll);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+
+        final Context context = PollActivity.this;
+
+
+        final Button button = findViewById(R.id.button_send);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                //mDatabase.child("test").setValue("WWW" + Math.random());
+
+                mDatabase.child("test/value").runTransaction(new Transaction.Handler() {
+                    @Override
+                    public Transaction.Result doTransaction(MutableData mutableData) {
+
+                        Integer currentValue = mutableData.getValue(Integer.class);
+                        if (currentValue == null) {
+                            mutableData.setValue(1);
+                        } else {
+                            mutableData.setValue(currentValue + 1);
+                        }
+
+                        return Transaction.success(mutableData);
+
+//                        if (p == null) {
+//                            Log.d("[][][][]", "BAD");
+//                            return Transaction.success(mutableData);
+//                        }
+
+//                        if (p.stars.containsKey(getUid())) {
+//                            // Unstar the post and remove self from stars
+//                            p.starCount = p.starCount - 1;
+//                            p.stars.remove(getUid());
+//                        } else {
+//                            // Star the post and add self to stars
+//                            p.starCount = p.starCount + 1;
+//                            p.stars.put(getUid(), true);
+//                        }
+
+                        // Set value and report transaction success
+                        //mutableData.setValue(p);
+                    }
+
+                    @Override
+                    public void onComplete(DatabaseError databaseError, boolean commited, DataSnapshot dataSnapshot) {
+                        Log.d(String.valueOf(Log.INFO), commited + "");
+                        Log.d("TAG", "postTransaction:onComplete:" + databaseError);
+                    }
+                });
+
+            }
+        });
+
     }
 }
+
---
+++ app/src/main/java/com/projects/deus_ex_machina/clustereducation/PollResult.java
@@ -0,0 +1,15 @@
+package com.projects.deus_ex_machina.clustereducation;
+
+public class PollResult {
+
+    public Integer value;
+
+    public PollResult() {
+        // Конструктор по умолчанию. Необходим для работы Firebase
+    }
+
+    public PollResult(Integer value) {
+        this.value = value;
+    }
+
+}
