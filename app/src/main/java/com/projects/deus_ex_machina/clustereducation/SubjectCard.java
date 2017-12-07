1b7ba4e9cb0e6b50453b7b2242e08442342a5efa
Michael Gursky <gursky.michael1999@gmail.com>
2017-12-07 19:50:57 +0200

    GridView edited slightly

---
+++ app/src/main/java/com/projects/deus_ex_machina/clustereducation/SubjectCard.java
@@ -0,0 +1,10 @@
+package com.projects.deus_ex_machina.clustereducation;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Michael on 12/7/2017
+ */
+
+public class SubjectCard  extends AppCompatActivity{
+}
