db4ef387729ab1d2866772e060aa0b67e911c4c2
Michael Gursky <gursky.michael1999@gmail.com>
2017-12-14 09:48:43 +0200

    code for subjects layout added

--- app/src/main/java/com/projects/deus_ex_machina/clustereducation/SubjectsFragment.java
+++ app/src/main/java/com/projects/deus_ex_machina/clustereducation/SubjectsFragment.java
@@ -6,13 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SubjectsFragment extends Fragment {
-
 
     public SubjectsFragment() {
         // Required empty public constructor
@@ -22,8 +25,31 @@ public class SubjectsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView;
+
+        rootView = inflater.inflate(R.layout.fragment_subject, container, false);
+
+        //Array of objects containing icons and subject names
+        ArrayList<SubjectCard> subjects = new ArrayList<>();
+
+        subjects.add(new SubjectCard("English", R.drawable.ic_subject_english));
+        subjects.add(new SubjectCard("History of Ukraine", R.drawable.ic_subject_history));
+        subjects.add(new SubjectCard("Intro to Specialty", R.drawable.ic_subject_intro_to_specialty));
+        subjects.add(new SubjectCard("Linear Algebra", R.drawable.ic_subject_linear_algebra));
+        subjects.add(new SubjectCard("Math Analysis", R.drawable.ic_subject_math_analysis));
+        subjects.add(new SubjectCard("Physics", R.drawable.ic_subject_physics));
+        subjects.add(new SubjectCard("Programming", R.drawable.ic_subject_programming));
+        subjects.add(new SubjectCard("Teamwork and presentation skills", R.drawable.ic_subject_teamwork));
+
+        SubjectCardAdapter subjectCardAdapter = new SubjectCardAdapter(getActivity(), subjects);
+
+        GridView subjectsView = rootView.findViewById(R.id.subject_grid_view);
+
+        subjectsView.setAdapter(subjectCardAdapter);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_subject, container, false);
+        //return inflater.inflate(R.layout.fragment_subject, container, false);
+        return subjectsView;
     }
 
 }
